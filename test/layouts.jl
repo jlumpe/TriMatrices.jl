496bd5bb4192e51d3aa1fabf7acd3891339685dd
Jared Lumpe <mjlumpe@gmail.com>
2021-02-21 19:39:23 -0700

    Reformat TriLayout tests

--- test/layouts.jl
+++ test/layouts.jl
@@ -5,31 +5,38 @@ using TriMatrices: trinum, hasdiag, nelems
 LAYOUT_TYPES = [TriLower, TriUpper, TriSymmetric]
 
 
-for L in LAYOUT_TYPES
+@testset "Type parameter" begin
+	for L in LAYOUT_TYPES
+		# Shouldn't throw any errors
+		L{true}()
+		L{false}()
+
+		# Default to true
+		@test typeof(L()) === L{true}
+
+		# Non-bool parameter
+		@test_throws ArgumentError L{Int}()
+	end
+end
+
+
+@testset "Methods and type traits" begin
 	n = 10
 
-	# Shouldn't throw any errors
-	L{true}()
-	L{false}()
+	for L in LAYOUT_TYPES
+		for D in [true, false]
+			layout = L{D}()
 
-	# Default to true
-	@test typeof(L()) === L{true}
+			@test hasdiag(layout) === D
+			@test hasdiag(typeof(layout)) === D
 
-	# Non-bool parameter
-	@test_throws ArgumentError L{Int}()
+			ne = trinum(D ? n : n - 1)
+			@test nelems(layout, n) == ne
+			@test nelems(typeof(layout), n) == ne
+		end
 
-	for D in [true, false]
-		layout = L{D}()
-
-		@test hasdiag(layout) === D
-		@test hasdiag(typeof(layout)) === D
-
-		ne = trinum(D ? n : n - 1)
-		@test nelems(layout, n) == ne
-		@test nelems(typeof(layout), n) == ne
+		# Type parameter needed
+		@test_throws MethodError hasdiag(L)
+		@test_throws MethodError nelems(L, n)
 	end
-
-	# Type parameter needed
-	@test_throws MethodError hasdiag(L)
-	@test_throws MethodError nelems(L, n)
 end
